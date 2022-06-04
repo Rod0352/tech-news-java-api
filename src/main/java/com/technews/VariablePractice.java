6970e9dbed177bf962bca5aafdd43a1b06d91e65
Rod0352 <92071426+Rod0352@users.noreply.github.com>
2022-06-03 23:36:18 -0400

    initial commit

---
+++ src/main/java/com/technews/VariablePractice.java
@@ -0,0 +1,12 @@
+package com.technews;
+
+public class VariablePractice {
+    public static void main(String[]args){
+        int sum1 = 32;
+        double sum2 = 7.99;
+        double sum = sum1+sum2;
+        String sum3 = "12";
+        String num = sum1 + sum3;
+        System.out.println(num);
+    }
+}
